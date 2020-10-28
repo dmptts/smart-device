72e18080653c610490913fd3bde3013c641afbf6
Dmitriy Solovev <dmptts@gmail.com>
2020-10-28 14:10:46 +0300

    Верстает главный экран

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,39 @@
 'use strict';
 
+(function () {
+  var MOBILE_WIDTH = 767;
+
+  var promoBtn = document.querySelector('.promo__btn');
+  var promoBtnDesktopText = 'Получить бесплатную консультацию';
+  var promoBtnMobileText = 'Бесплатная консультация';
+  var currentVersion;
+
+  var getCurrentVersion = function () {
+    if (document.body.clientWidth <= MOBILE_WIDTH) {
+      currentVersion = 'mobile';
+    } else {
+      currentVersion = 'desktop';
+    }
+  };
+
+  var switchText = function () {
+    if (currentVersion === 'mobile') {
+      promoBtn.textContent = promoBtnMobileText;
+    } else {
+      promoBtn.textContent = promoBtnDesktopText;
+    }
+  };
+
+  window.addEventListener('resize', function (evt) {
+    evt.preventDefault();
+    var pastVersion = currentVersion;
+    getCurrentVersion();
+
+    if (pastVersion !== currentVersion) {
+      switchText();
+    }
+  });
+
+  getCurrentVersion();
+  switchText();
+})();
